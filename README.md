96120ab61a326c47885e429284e1243b2a13280a
Maximilian Mikus <maximilianmikus@gmail.com>
2019-04-12 11:05:10 +0200

    LDP-160 Use vue-cli and update to new specs.

--- README.md
+++ README.md
@@ -12,7 +12,23 @@ via npm:
 import it:
 
 ```
-import { PgLink } from 'vue-lupus-paragraph-link';
+import PgLink from 'vue-lupus-paragraph-link';
 
 Vue.component('pg-link', PgLink);
+```
+
+## Slots
+You can use the following slots
+
+- `title` ( optional )
+  A title.
+- `link` ( default )
+  The link
+
+## Example
+```
+<pg-link>
+  <h3 slot="title">The title value</h3>
+  <a href="#the-url" slot="link">The link label</a>
+</pg-link>
 ```
--- src/pg-link.vue
+++ src/pg-link.vue
@@ -1,29 +1,11 @@
 <template>
   <div class="paragraph paragraph--link">
-    <div class="paragraph__field-link">
-      <slot name="field_link">
-        <a
-          class="paragraph__link"
-          v-for="link in linksComputed"
-          :href="link.uri"
-        >
-          {{ link.title }}
-        </a>
-      </slot>
-      <slot/>
-    </div>
+    <slot name="title" />
+    <slot :name="$slots.link ? 'link': 'default'" />
   </div>
 </template>
 <script>
   export default {
-    name: 'PgLink',
-    props: {
-      dataLinks: { type: String, default: () => '[]' },
-    },
-    computed: {
-      linksComputed() {
-        return JSON.parse(this.dataLinks);
-      },
-    }
+    name: 'PgLink'
   };
 </script>
